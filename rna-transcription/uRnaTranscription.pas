30449030b99464c99f7aac4b95a15ce762e0f15b
Ryan Potts <rpottsoh@gmail.com>
2017-04-08 22:20:55 -0400

    rna-transcription

---
+++ rna-transcription/uRnaTranscription.pas
@@ -0,0 +1,58 @@
+unit uRnaTranscription;
+
+interface
+uses Generics.Collections;
+
+type
+  complement = class
+  private
+    class var
+      fDnaToRna: TDictionary<char, char>;
+  public
+    class function OfDna(nucleotide: string): string; static;
+  end;
+
+implementation
+uses SysUtils;
+
+class function complement.OfDna(nucleotide: string): string;
+var i: integer;
+    lRNA: string;
+    elements: TArray<char>;
+    RNAelement: char;
+begin
+  result := '';
+  lRNA := '';
+  if not trim(nucleotide).IsEmpty then
+  begin
+    if not assigned(fDNAToRna) then
+    begin
+      fDnaToRna := TDictionary<char, char>.Create;
+      with fDnatoRna do
+      begin
+        add('G','C');
+        add('C','G');
+        add('T','A');
+        add('A','U');
+      end;
+    end;
+    elements := nucleotide.ToUpper.ToCharArray;
+    i := 0;
+    while i <= high(elements) do
+    begin
+      if fDnaToRna.TryGetValue(elements[i],RNAelement) then
+      begin
+        lRNA := lRNA + RNAelement;
+        inc(i);
+      end
+      else
+      begin
+        lRNA := '';
+        break;
+      end;
+    end;
+  end;
+  result := lRNA;
+end;
+
+end.
---
+++ rna-transcription/uTestRnaTranscription.pas
@@ -0,0 +1,89 @@
+unit uTestRnaTranscription;
+
+interface
+uses
+  DUnitX.TestFramework;
+
+type
+  [TestFixture]
+  RnaTranscriptionTest = class(TObject)
+  public
+    [Test]
+//  [Ignore('Comment the "[Ignore]" statement to run the test')]
+    procedure Rna_complement_of_cytosine_is_guanine;
+
+    [Test]
+//    [Ignore]
+    procedure Rna_complement_of_guanine_is_cytosine;
+
+    [Test]
+//    [Ignore]
+    procedure Rna_complement_of_thymine_is_adenine;
+
+    [Test]
+//    [Ignore]
+    procedure Rna_complement_of_adenine_is_uracil;
+
+    [Test]
+//    [Ignore]
+    procedure Rna_complement;
+
+    [Test]
+//    [Ignore]
+    procedure Dna_correctly_handles_invalid_input;
+
+    [Test]
+//    [Ignore]
+    procedure Dna_correctly_handles_completely_invalid_input;
+
+    [Test]
+//    [Ignore]
+    procedure Dna_correctly_handles_partially_invalid_input;
+  end;
+
+implementation
+uses uRnaTranscription;
+
+procedure RnaTranscriptionTest.Rna_complement_of_cytosine_is_guanine;
+begin
+  Assert.AreEqual('G', complement.OfDna('C'));
+end;
+
+procedure RnaTranscriptionTest.Rna_complement_of_guanine_is_cytosine;
+begin
+  Assert.AreEqual('C', complement.OfDna('G'));
+end;
+
+procedure RnaTranscriptionTest.Rna_complement_of_thymine_is_adenine;
+begin
+  Assert.AreEqual('A', complement.OfDna('T'));
+end;
+
+procedure RnaTranscriptionTest.Rna_complement_of_adenine_is_uracil;
+begin
+  Assert.AreEqual('U', complement.OfDna('A'));
+end;
+
+procedure RnaTranscriptionTest.Rna_complement;
+begin
+  Assert.AreEqual('UGCACCAGAAUU', complement.OfDna('ACGTGGTCTTAA'));
+end;
+
+procedure RnaTranscriptionTest.Dna_correctly_handles_invalid_input;
+begin
+  Assert.AreEqual('', complement.OfDna('U'));
+end;
+
+procedure RnaTranscriptionTest.Dna_correctly_handles_completely_invalid_input;
+begin
+  Assert.AreEqual('', complement.OfDna('XXX'));
+end;
+
+procedure RnaTranscriptionTest.Dna_correctly_handles_partially_invalid_input;
+begin
+  Assert.AreEqual('', complement.OfDna('ACGTXXXCTTAA'));
+end;
+
+initialization
+  TDUnitX.RegisterTestFixture(RnaTranscriptionTest);
+end.
