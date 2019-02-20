82db149733edef7349dc9c1720fc2d225b40ee47
Ryan Potts <rpottsoh@gmail.com>
2019-02-19 21:44:45 -0500

    Resistor-Color: simplify solution

--- delphi/resistor-color/uResistorColor.pas
+++ delphi/resistor-color/uResistorColor.pas
@@ -3,10 +3,14 @@ unit uResistorColor;
 interface
 
 type
-  TResistor = class
+  TColorCodes = TArray<string>;
+
+type
+  TResistor = class(TObject)
   private
-    const colorCodes: array[0..9] of string = ('black', 'brown', 'red',
-      'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white');
+    const
+      ColorCodes: TColorCodes = ['black', 'brown', 'red',
+      'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
   public
     class function colorCode(aColor: string): integer;
     class function colors: TArray<string>;
@@ -20,16 +24,14 @@ uses
 
 class function TResistor.colorCode(aColor: string): integer;
 begin
-  var colorList := TList<string>.Create;
-  colorList.AddRange(colorCodes);
-  result := colorList.IndexOf(aColor);
+  var ColorList := TList<string>.Create;
+  ColorList.AddRange(ColorCodes);
+  result := ColorList.IndexOf(aColor);
 end;
 
 class function TResistor.colors: TArray<string>;
 begin
-  var colorList := TList<string>.Create;
-  colorList.AddRange(colorCodes);
-  result := colorList.ToArray;
+  result := ColorCodes;
 end;
 
 end.
