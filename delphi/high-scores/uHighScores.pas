8251d357cb6bdd0903528cc00b1e4c7e25afe1f4
Ryan Potts <rpottsoh@gmail.com>
2019-02-22 21:31:45 -0500

    high-scores: update solution for new test suite
    exercise was updated to more closely follow topic of Lists.

--- delphi/high-scores/uHighScores.pas
+++ delphi/high-scores/uHighScores.pas
@@ -1,44 +1,52 @@
 unit uHighScores;
 
 interface
+uses
+  System.Generics.Collections;
 
 type
   IScores = interface(IInvokable)
   ['{3D174C80-BA8D-4D92-AB1B-DA29ED0812CE}']
-    function Input(AInput: TArray<integer>): ISCores;
-    function Scores:  TArray<integer>;
+    function Scores:  TList<integer>;
     function Latest: integer;
     function Highest: integer;
-    function personalTopThree: TArray<integer>;
+    function personalTopThree: TList<integer>;
     function Report: string;
   end;
 
-function NewScores: IScores;
+function NewScores(const aScores: TArray<integer>): IScores;
 
 implementation
 
 uses
-  System.Generics.Collections, System.SysUtils;
+  System.SysUtils;
 
 type
 
   TScores = class(TInterfacedObject, IScores)
   private
-    FScores: TArray<integer>;
+    FScores: TList<integer>;
   public
-    function Input(AInput: TArray<integer>): ISCores;
-    function Scores:  TArray<integer>;
+    constructor Create(const aScores: TArray<integer>);
+    destructor Destroy; override;
+    function Scores: TList<integer>;
     function Latest: integer;
     function Highest: integer;
-    function personalTopThree: TArray<integer>;
+    function personalTopThree: TList<integer>;
     function Report: string;
   end;
 
  { TScores }
 
-function NewScores: IScores;
+function NewScores(const aScores: TArray<integer>): IScores;
 begin
-  result := TScores.Create;
+  result := TScores.Create(aScores);
+end;
+
+destructor TScores.Destroy;
+begin
+  FScores.DisposeOf;
+  inherited;
 end;
 
 function TScores.Highest: integer;
@@ -49,33 +57,37 @@ begin
   Result := lScores.Last;
 end;
 
-function TScores.Input(AInput: TArray<integer>): ISCores;
+constructor TScores.Create(const aScores: TArray<integer>);
 begin
-  FScores := AInput;
-  Result := self;
+  inherited Create;
+  FScores := TList<integer>.Create;
+  FScores.AddRange(aScores);
 end;
 
 function TScores.Latest: integer;
 begin
-  Result := FScores[High(FScores)];
+  Result := FScores.Last;
 end;
 
 function TScores.Report: string;
+const
+  cPersonalBest = 'Your latest score was %d. That''s your personal best!';
+  cShortOfPersonalBest = 'Your latest score was %d. That''s %d short of your personal best!';
 begin
   var lLatest := Latest;
   var lHighest := Highest;
   if lLatest = lHighest then
-    Result := format('Your latest score was %d. That''s your personal best!',[lLatest])
+    Result := format(cPersonalBest,[lLatest])
   else
-    Result := format('Your latest score was %d. That''s %d short of your personal best!',[lLatest, lHighest - lLatest]);
+    Result := format(cShortOfPersonalBest,[lLatest, lHighest - lLatest]);
 end;
 
-function TScores.Scores: TArray<integer>;
+function TScores.Scores: TList<integer>;
 begin
   Result := FScores;
 end;
 
-function TScores.personalTopThree: TArray<integer>;
+function TScores.personalTopThree: TList<integer>;
 begin
   var lScores := TList<integer>.Create;
   lScores.AddRange(FScores);
@@ -83,7 +95,7 @@ begin
   lScores.Reverse;
   if lScores.Count > 3 then
     lScores.Count := 3;
-  Result := lScores.ToArray;
+  Result := lScores;
 end;
 
 end.
